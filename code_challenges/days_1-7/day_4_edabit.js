d0d4c7b6703644ce2bdabeaaba80520e4aaf99e2
melonybox <grandmadcow@gmail.com>
2020-09-25 00:38:24 -0400

    day 4

--- code_challenges/days_1-7/day_4_edabit.js
+++ code_challenges/days_1-7/day_4_edabit.js
@@ -59,3 +59,70 @@ function totalAmountAdjectives(obj) {
 function endCorona(recovers, newCases, activeCases) {
 	return Math.ceil(activeCases/(recovers - newCases))
 }
+
+let promise = new Promise( (resolve) => {
+	let animal = "cat"
+  setTimeout(() => {
+		if(animal === "dog") {
+			resolve("It's a dog!")
+		}
+	  if(animal !== "dog") {
+			reject("It's not a dog!")
+		}
+  }, 1000)
+})
+
+function smashFactor(bs, cs) {
+	return parseFloat((bs/cs).toFixed(2))
+}
+
+const REGEXP = /\b\d\d:\d\d\b/
+
+function getFillings(sandwich) {
+	return sandwich.slice(1,-1)
+}
+
+function isEven(n) {
+	return !(n % 2)
+}
+
+function makeCopy(obj) {
+	return {...obj}
+}
+
+function hasSameBread(arr1, arr2) {
+	const bread1 = Array.from(new Set([arr1[0],arr1[arr1.length-1]]))
+	const bread2 = Array.from(new Set([arr2[0],arr2[arr2.length-1]]))
+
+	if (bread1.length !== bread2.length){
+		return false
+	}
+
+	return JSON.stringify(bread1) === JSON.stringify(bread2)
+}
+
+// return arr1.shift() === arr2.shift() && arr1.pop() === arr2.pop();
+
+function isSafeBridge(str) {
+	return !str.includes(" ")
+}
+
+function countUnique(s1, s2) {
+	return Array.from(new Set(s1 + s2)).length
+}
+
+// return new Set(s1 + s2).size;
+// The size accessor property returns the number of (unique) elements in a Set object.
+
+function and(a, b) {
+	return a && b
+}
+
+function calculate(num1, num2, op) {
+	return eval(num1+op+num2)
+}
+
+function mean(nums) {
+	sumNums = nums.reduce((sum, x) => sum + x)
+	return parseFloat((sumNums/nums.length).toFixed(1))
+}
